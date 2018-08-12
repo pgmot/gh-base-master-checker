bfe01d7917a3f07b89d892294cc7ff9bc43b50a2
mot <mot.contact.address.pc@gmail.com>
2018-08-13 01:53:28 +0900

    完成

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,11 +1,33 @@
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.color) {
-        console.log('Receive color = ' + msg.color);
-        document.body.style.backgroundColor = msg.color;
-        sendResponse('Change color to ' + msg.color);
-    } else {
-        sendResponse('Color message is none.');
+import * as $ from 'jquery';
+
+function isBaseBranchMaster(): boolean {
+    return $('span.base-ref > span')[0].textContent === "master";
+};
+
+function isAlreadyAddedWarningMessage(): boolean {
+    return $('div.gbmc-warning-message').length > 0;
+}
+
+function addWarningMessage(): void {
+    const messageDiv = $('<div class="gbmc-warning-message" style="color: red; font-weight: 900;">This Pull Request is not merge to master!!!!</div>');
+    $('div.merge-message').css("background", "red");
+    $('div.branch-action-body').append(messageDiv);
+};
+
+let timer = null;
+$(document).on('DOMNodeInserted', () => {
+    const urlRegexp = /https:\/\/github\.com\/(.*)\/pull\//;
+
+    if (!location.href.match(urlRegexp)) {
+        return;
+    }
+
+    if (isAlreadyAddedWarningMessage()) {
+        return;
+    }
+
+    if (!isBaseBranchMaster()) {
+        addWarningMessage();
     }
 });
-
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        popup: path.join(__dirname, '../src/popup.ts'),
-        options: path.join(__dirname, '../src/options.ts'),
-        background: path.join(__dirname, '../src/background.ts'),
         content_script: path.join(__dirname, '../src/content_script.ts')
     },
     output: {
